8d99643b37a417ccd4b872862ba33c502451e3dc
shailesh.gupta <shailesh.gupta@veritas.com>
2019-06-19 21:37:07 +0530

    "Added validation testcases for all apis and also change the log directory path in log4j2 and updated the testsuite file

--- src/test/java/com/qa/restapiproject/JsonPlaceHolderApiTest.java
+++ src/test/java/com/qa/restapiproject/JsonPlaceHolderApiTest.java
@@ -3,6 +3,7 @@ package com.qa.restapiproject;
 import static org.testng.Assert.fail;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import org.apache.logging.log4j.LogManager;
@@ -23,7 +24,9 @@ public class JsonPlaceHolderApiTest {
 	JsonPlaceHolderApi jsonPlaceHolderApi = new JsonPlaceHolderApi() ; 
 	EmailAddressValidator emailValidator= new EmailAddressValidator();
 	private static final Logger logger = LogManager.getLogger(JsonPlaceHolderApiTest.class);
-
+	String  userId;
+	List<Integer> postIds;
+	List <Map<String,?>>allCommentsforpostId ;
 	/**
 	 * @param userName
 	 * @param baseUrl
@@ -38,25 +41,25 @@ public class JsonPlaceHolderApiTest {
 		logger.info("\n################### The "+context.getName()+ " Test: Started ###############\n");
 
 		RestAssured.baseURI=baseUrl;
-		List <Map<String,?>>allCommentsforpostId ;
+
 		try {
 
 
 			//This method call will return the Id of the  given user
 			logger.info("\n--------Getting the UserID of user *"+userName+"*---------------------- \n");
 
-			
-			String  userId= jsonPlaceHolderApi.getUserid(userName);
+
+			userId= jsonPlaceHolderApi.getUserid(userName);
 			Assert.assertTrue(!(userId.isEmpty()),"The userId not found for user: "+userName);
 
 			//This method call will return the list of Ids of all the posts posted by given user
 			logger.info("--------Getting all the postsIDs for the posts written by user *"+userName+"* having UserId *"+userId+"*-------------- \n");
-			List<Integer> postIds= jsonPlaceHolderApi.getPostIds(userId);
+			postIds= jsonPlaceHolderApi.getPostIds(userId);
 			Assert.assertTrue(!(postIds.isEmpty()), "No posts found for user with userID: "+ userId);
 
 			/*	This for each loop will iterate over each post Ids and will fetch comments Id and EmailId
 				Related to each post and will validate the each email ID*/
-			
+
 			logger.info("---------Fetching the comments for each PostIds and Validating the emailId Format------------");
 			for(int postId: postIds)
 			{
@@ -97,12 +100,100 @@ public class JsonPlaceHolderApiTest {
 		}
 		catch (Exception e)
 		{
-			e.printStackTrace();
+			logger.error(e.getStackTrace());
 			fail("\n ####### The "+context.getName()+" Test: Failed with Exception : ############" +e.getMessage());
-			
+
 		}
-		
+
 
 
 	}
+
+
+	/**
+	 * This test method is written to validate the response from Users Api. 
+	 * We can add more validation in this method by varying the parameters and validating on other fields
+	 * We can also use data provider or some file to get the data
+	 * @param userName
+	 * @param context
+	 */
+	@Test 
+	@Parameters ({"userName", "expectedUserId","baseUrl"})
+	public void validateUsersApiResponse(String userName,String expectedUserId,String baseUrl,ITestContext context)
+	{
+		logger.info("\n######### " + context.getName()+ " Started: ##########\n");
+		RestAssured.baseURI=baseUrl;
+
+		try {
+			userId= jsonPlaceHolderApi.getUserid(userName);
+			Assert.assertEquals(userId,expectedUserId, "Invalid response from users Api ");
+		}
+
+		catch (Exception e)
+		{
+			logger.error(e.getStackTrace());
+			fail("\n ####### The "+context.getName()+" Test: Failed with Exception : ############" +e.getMessage());
+		}
+
+
+	}
+
+	/**
+	 * This test method is written to validate the response from posts Api. 
+	 * We can add more validation in this method by varying the parameters and validating on other fields
+	 * We can also use data provider or some file to get the data
+	 * @param userId
+	 * @param expectedPostIds
+	 * @param context
+	 */
+	@Test 
+	@Parameters ({"userId", "expectedPostIdCount","baseUrl"})
+	public void validatePostsApiResponse(String userId, int expectedPostIdCount,String baseUrl, ITestContext context )
+	{
+		logger.info("\n######### " + context.getName()+ " Started: ##########\n");
+		RestAssured.baseURI=baseUrl;
+		
+		try {
+			postIds=  jsonPlaceHolderApi.getPostIds(userId);
+			Assert.assertEquals(postIds.size(),expectedPostIdCount, "Invalid response from posts Api ");
+		}
+
+		catch (Exception e)
+		{
+			logger.error(e.getStackTrace());
+			fail("\n ####### The "+context.getName()+" Test: Failed with Exception : ############" +e.getMessage());
+		}
+
+	}
+
+	/**
+	 * This test method is written to validate the response from Comments Api. 
+	 * We can add more validation in this method by varying the parameters and validating on other fields
+	 * We can also use data provider or some file to get the data
+	 * @param postId
+	 * @param expectedTotalComments
+	 * @param context
+	 */
+	@Test 
+	@Parameters ({"postId", "expectedTotalComments","baseUrl"})
+	public void validateCommentsApiResponse(int postId, int expectedTotalComments,String baseUrl, ITestContext context )
+	{
+		logger.info("\n######### " + context.getName()+ " Started: ##########\n");
+		RestAssured.baseURI=baseUrl;
+
+		try {
+			allCommentsforpostId=  jsonPlaceHolderApi.getCommentsForPostId(postId);
+			Assert.assertEquals(allCommentsforpostId.size(),expectedTotalComments, "Invalid response from comments  Api ");
+		}
+
+		catch (Exception e)
+		{
+			logger.error(e.getStackTrace());
+			fail("\n ####### The "+context.getName()+" Test: Failed with Exception : ############" +e.getMessage());
+		}
+
+	}
+
+
+
 }
